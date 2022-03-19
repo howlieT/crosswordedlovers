99b4895e45eb7ca12ff5b9d4cb9791a0e087e6d3
howlieT <misha.anker@gmail.com>
2022-03-19 10:15:04 +0000

    Attempts to fix

---
+++ ItemDicts.py
@@ -0,0 +1,21 @@
+# Kitchen
+# Hall
+dict_of_all_items = {
+                        "coat_stand_dict": {"look coat stand": "A wooden stand for hanging coats on, "
+                                            "maybe hats and scarves too. You can put umbrellas in the base.",
+                                            "use coat stand": "You hang your jacket on the coat stand",
+                                            "break coat stand": "You break the coat stand, it's broken now."},
+                        "key_hook_dict": {"look key hook": "Hooks for hanging keys on, "
+                                          "there's a spare set here for the front door.",
+                                          "use key hook": "You hang up your keys.",
+                                          "take key": "You take a set of keys off the hook.",
+                                          "break key hook": "You break the key hook, its broken now"},
+                        "doormat_dict": {"look doormat":
+                                         "A scratchy brown mat for getting the dirt off your shoes when you come in.",
+                                         "use doormat": "You scrape your shoes off on the mat, nice and clean now.",
+                                         "break doormat": "You try to rip the doormat, nothing really happens. "
+                                         "You try and pull some bits out, your fingers hurt now."}
+                    }
+# Bedroom
+# Bathroom
+# Garden
